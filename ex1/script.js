33b933f70e54af01732ba0e3a073398c75747397
Arver1 <aactepanov@mail.ru>
2018-07-25 06:06:25 +0300

    added not-standard shadow

--- ex1/script.js
+++ ex1/script.js
@@ -1,17 +1,18 @@
-let imgs = document.querySelectorAll('.ex1 div');
+const imgs = document.querySelectorAll('.ex1__c');
+const btn = document.querySelector('.ex1 button');
 btn.addEventListener('click', function(){
 	showImg(imgs);
 });
 function showImg(cll){
-	if(showImg.count == cll.length) {
+	if(showImg.count === cll.length) {
 		showImg.count = 0;
 	}
-	if(showImg.count == 0) {
+	if(showImg.count === 0) {
 		showImg.arr = Array.prototype.slice.call(cll);
 	}
 	let currentImg = generateId(showImg.arr).next().value;
-	if(showImg.hidden) showImg.hidden.classList.add('hidden');
-	currentImg.classList.remove('hidden');
+	if(showImg.hidden) showImg.hidden.classList.add('ex1__c--hidden');
+	currentImg.classList.remove('ex1__c--hidden');
 	showImg.count++;
 	showImg.hidden = currentImg;
 }
@@ -21,6 +22,6 @@ function* generateId(arr) {
 	yield arr.splice(getRandomNumber(0, arr.length-1), 1)[0];
 }
 function getRandomNumber(min, max) {
-	if(max == 0) return 0;
+	if(max === 0) return 0;
 	return Math.floor(min + Math.random() * (max + 1 - min));
 }
